9898977081f0db61efbafa1d1ec6833ea6befa0b
hoseony <hoseon.alex.yu@gmail.com>
2026-06-06 01:54:54 -0400

    flashing works like wow

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -1,12 +1,10 @@
 #include <stdint.h>
 
 // ------------ function prototypes -------------
-int main(void);
 // ----------------------------------------------
 
-extern void _estack(void); // stm32f407.ld
-
-/* __attribute__()
+/* SOME NOTES...
+ * 1. __attribute__()
  *
  * __attribute__((__aligned__(x)))
  * it aligns memory address to multiple of x 
@@ -18,26 +16,38 @@ extern void _estack(void); // stm32f407.ld
  * It forces stuff into a specific section in the object file
  * 
  * There are more stuff you can do, but I only put here things that might useful to me
+ *  - naked: omit standard function prologue and epilogue sequence (when generating asesembly)
+ *  - noreturn: this function never returns
  */
 
-// Startup code
-__attribute__((naked, noreturn)) void _reset(void) {
-  // memset .bss to zero, and copy .data section to RAM region
-  extern long _sbss, _ebss, _sdata, _edata, _sidata;
-  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+int main(void) {
+    uint32_t cnt = 0;
+    uint32_t half;
 
-  main();             // Call main()
-  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+    while(1) {
+        cnt += 2;
+        half = cnt / 2;
+        ++half;
+    }
+}
+
+extern void _estack(void); // stm32f407.ld
+
+// Startup code
+
+// _reset handler
+__attribute__((naked, noreturn)) void _reset(void) {
+    // we have defined these variables in stm32f407.ld
+    extern long _sbss, _ebss, _sdata, _edata, _sidata;
+
+    // setting bss to 0
+    for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+    // copying .data initialized in flash to .data in memory
+    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+    main();             // Call main()
+    for (;;) (void) 0;  // Infinite loop in the case if main() returns
 }
 
 // 16 standard and 91 STM32-specific handlers
 __attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {_estack, _reset};
-
-int main() {
-    while(1) {
-
-    }
-}
-
-
