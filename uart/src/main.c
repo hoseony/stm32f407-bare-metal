88d3e1c40a8979e352c6f00de6415ee8907af326
hoseony <hoseon.alex.yu@gmail.com>
2026-06-20 16:30:24 -0400

    added ring buffer functions

--- uart/src/main.c
+++ uart/src/main.c
@@ -3,10 +3,11 @@
 #include "../include/utils.h"
 #include "../include/uart.h"
 
+// ============ Function Prototype =============
 
 bool checkMatch(uint8_t *buf, uint8_t *bytes, uint32_t size);
 
-// ------------ Main Instructions -------------
+// ========== Main Instructions ============
 /* note
  *  maybe I should make a function for this:
  *      GPIO_t *gpio = GPIO(PINBANK(pin));
@@ -28,6 +29,7 @@ void GPIO_setAF(uint16_t pin, uint8_t AFnum) {
         gpio->AFRL |= ((uint32_t)AFnum << (num * 4));
     }
 }
+
 /* For now, I might not need to do this? I'll see how it goes 
  * (I am thinking perhasp default is fine)
 
@@ -65,9 +67,44 @@ void GPIO_OSPEEDR_writeBit(uint16_t pin, uint8_t val) {
 }
 */
 
+// ========== RingBuffer implementation ==========
+#define RINGBUFFER_SIZE 64
+
+typedef struct {
+    uint8_t data[RINGBUFFER_SIZE];
+    uint32_t head;
+    uint32_t tail;
+} RINGBUFFER_t; 
+
+// return byte through *byte
+bool RING_pop(RINGBUFFER_t *rb, uint8_t *byte) {
+    if (rb->head == rb->tail) { // buffer empty, nothing to free
+        return false;
+    }
+
+    *byte = rb->data[rb->head]; // return the value at the head
+    rb->head = (rb->head + 1) % RINGBUFFER_SIZE; // move the head (% for wraparround)
+    return true;
+}
+
+bool RING_push(RINGBUFFER_t *rb, uint8_t byte) {
+    // % RINGBUFFER_SIZE in case wraparround
+    uint32_t nextTail = (rb->tail + 1) % RINGBUFFER_SIZE;
+
+    if (nextTail == rb->head) { // buffer is full
+        return false;
+    }
+
+    rb->data[rb->tail] = byte;
+    rb->tail = nextTail;
+    return true;
+}
+
+// ===============================================
+
 int main(void) {
     
-    // blinking setup
+    // ========== blinky ==========
     uint16_t LED_green = PIN('D', 12);
     uint16_t LED_red = PIN('D', 14);
 
@@ -80,8 +117,7 @@ int main(void) {
 
     uint32_t timer_green= 0;
     uint32_t period = 500;
-     
-    // USART
+    // ========== USART ==========
     // Let's use these for now
     //  - PA0 AF8, UART4_TX
     //  - PA1 AF8, UART4_RX
@@ -101,12 +137,15 @@ int main(void) {
     GPIO_setAF(uart_rx, 8);
 
     UART4->CR1 = 0;
-    UART4->BRR = 16000000 / 115200; // This is actually not exact because we are leaving the fraction part for now
-    UART4->CR1 = BIT(3) | BIT(2) | BIT(13);
+    UART4->BRR = 16000000 / 115200; // BaudRateRegister
+    // This is actually not exact because we are leaving the fraction part for now
 
-    // Main Loop
+    UART4->CR1 = BIT(3) | BIT(2) | BIT(13);
+    
+    // ========= Main Loop ==========
     while (1) {
-        /*
+        /* e.g. testing transmission/reception of a byte
+        
         if (timer_expired(&timer_green, period, s_ticks)) {
             static bool LED_green_bool = false;
             static bool LED_red_bool = false;
@@ -123,7 +162,8 @@ int main(void) {
                 LED_red_bool = !LED_red_bool;
             }
         }
-        */
+        * Yeah, this works, but multiple bytes would be cooler */
+
         if (timer_expired(&timer_green, period, s_ticks)) {
             static bool LED_green_bool = false;
             static bool LED_red_bool = true;
@@ -135,11 +175,17 @@ int main(void) {
             uint32_t size = sizeof(bytes) - 1;
             uint8_t buf[sizeof(bytes) - 1];
 
-            for (uint32_t i = 0; i < size; i++) {
-                UART_transmitByte(UART4, bytes[i]);
-            }
+            // Let's make interrupt based uart communication
+            // As mentioned on the comment of the commit, 
+            // The hardware can only hold 1 byte, so I can not send multiple bytes 
+            // and hope it is stored inside some FIFO register 
+            
+            // For that, let's head to Reference Manual 30.4 USART interrupts
+            // There is RXNE (received Data Ready to be Read)
+            // Once this event happens, we immidetly need to read the data
 
-            UART_readBytes(UART4, buf, size);
+            // One way of achieving this is by using "ring buffer" to store incoming data.
+            
 
             if (checkMatch(bytes, buf, size)) {
                 LED_green_bool = !LED_green_bool;
--- uart/uart.md
+++ uart/uart.md
@@ -1,1 +1,14 @@
-# 02 UART`` 
+# 02 UART 
+
+
+## Single Byte
+
+## Testing
+
+## Multiple Bytes
+Let's make interrupt based uart communication. As mentioned on the comment of the commit, the hardware can only hold 1 byte. Thus, I can not send multiple bytes and hope it is stored inside some FIFO register.
+
+To fix this, let's head to Reference Manual 30.4 USART interrupts. There is RXNE (received Data Ready to be Read) once this event happens, we immidetly need to read the data. Since this is an interrupt requests, things will work nicely.
+
+One way of storing data is thorugh "ring buffer". I believe other people explained it [better](https://ntietz.com/blog/whats-in-a-ring-buffer/).
+
